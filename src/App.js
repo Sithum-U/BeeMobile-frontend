87eee898ae3b7e53105d002fbf4d191795e17c9f
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2022-08-30 12:06:32 +0530

    payment form creates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import checkout from "./components/PurchaseOrder/checkout";
+import PaymentForm from "./components/PurchaseOrder/PaymentForm";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/checkout" exact element={<checkout />} />
+        {/* <Route exact path="/checkout" component={checkout} /> */}
+        <Route path="/PaymentForm" exact element={<PaymentForm />} />
       </Routes>
     </BrowserRouter>
   );
