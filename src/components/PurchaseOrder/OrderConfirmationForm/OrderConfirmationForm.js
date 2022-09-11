fbb25ba497fa01c1b64993076dcb97b5eec3345a
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2022-09-11 07:30:11 +0530

    Order Confirmation eddited

--- src/components/PurchaseOrder/OrderConfirmationForm/OrderConfirmationForm.js
+++ src/components/PurchaseOrder/OrderConfirmationForm/OrderConfirmationForm.js
@@ -1,79 +1,193 @@
-import * as React from "react";
-import Typography from "@mui/material/Typography";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import Grid from "@mui/material/Grid";
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Grid from "@material-ui/core/Grid";
+
+// import jspdf from "jspdf";
+// import "jspdf-autotable";
+// import img from "../components/wsLogo.jpg";
+// import img1 from "../components/signature.jpg";
 
 import "./orderConfirmation.css";
+const rows = [
+  {
+    id: "itemName",
+    label: "Item Name",
+    minWidth: 100,
+    align: "center",
+    main: "#f44336",
+  },
+  {
+    id: "itemPrice",
+    label: "Item Price",
+    minWidth: 100,
+    align: "center",
+    main: "#f44336",
+  },
+  {
+    id: "itemQuantity",
+    label: "Item Quantity",
+    minWidth: 100,
+    align: "center",
+    main: "#f44336",
+  },
+  {
+    id: "totalItemPrice",
+    label: "Total Item Price",
+    minWidth: 170,
+    align: "center",
+    // format: (value) => value.toLocaleString('en-US'),
+  },
+];
 
 const products = [
-  {
-    name: "Product 1",
-    desc: "A nice thing",
-    price: "$9.99",
-  },
-  {
-    name: "Product 2",
-    desc: "Another thing",
-    price: "$3.45",
-  },
-  {
-    name: "Product 3",
-    desc: "Something else",
-    price: "$6.51",
-  },
-  {
-    name: "Product 4",
-    desc: "Best thing of all",
-    price: "$14.11",
-  },
-  { name: "Shipping", desc: "", price: "Free" },
+  { name: "", desc: "", price: "" },
+  { name: "Delivery Type", desc: "", price: "Free" },
 ];
-
-const addresses = ["1 MUI Drive", "Reactville", "Anytown", "99999", "USA"];
+const addresses = ["No 386", "Kandy", "Road", "Nittabuwa", "SRILANKA"];
 const payments = [
-  { name: "Card type", detail: "Visa" },
-  { name: "Card holder", detail: "Mr John Smith" },
-  { name: "Card number", detail: "xxxx-xxxx-xxxx-1234" },
-  { name: "Expiry date", detail: "04/2024" },
+  { name: "Card Type", detail: "Credit Card" },
+  { name: "Card Number", detail: "6527 1332 4286 2542" },
+  { name: "Card Holder Name", detail: "Amerasinghe N.D.K" },
+  { name: "CVV code", detail: "216" },
+  { name: "Expiry Date", detail: "12/24" },
 ];
 
-export default function Review() {
+const useStyles = makeStyles((theme) => ({
+  listItem: {
+    padding: theme.spacing(1, 0),
+  },
+  total: {
+    fontWeight: 700,
+  },
+  title: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+export default function POFinalReviewForm() {
+  const classes = useStyles();
+
+  const [poRequestManage, setpoRequestManage] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filtered, setfiltered] = useState([]);
+  const [itemName, setitemName] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:9000/poroutes/")
+      .then((res) => res.json())
+      .then((data) => {
+        setpoRequestManage(data);
+      });
+  }, []);
+
+  const sum = [];
+
+  // genarate pdf
+
+  // const generatePDF = (tickets) => {
+  //   const doc = new jspdf();
+  //   const tableColumn = [
+  //     "order ID",
+  //     "Item Name",
+  //     "Item Price",
+  //     "Item Quantity",
+  //     "Item Price",
+  //   ];
+  //   const tableRows = [];
+  //   const date = Date().split(" ");
+  //   const dateStr = date[1] + "-" + date[2] + "-" + date[3];
+
+  //   tickets.map((ticket) => {
+  //     const ticketData = [
+  //       ticket._id,
+  //       ticket.itemName,
+  //       ticket.itemPrice,
+  //       ticket.itemQuantity,
+  //       ticket.itemPrice * ticket.itemQuantity,
+  //     ];
+  //     tableRows.push(ticketData);
+  //   });
+  //   doc.text("WICKRAMA SUPER PLC", 70, 8).setFontSize(13);
+  //   doc.text("Purchase Order Detail Report", 14, 16).setFontSize(13);
+  //   doc.text(`Report Genarated Date - ${dateStr}`, 14, 23);
+  //   //right down width height
+  //   doc.addImage(img, "JPEG", 170, 8, 25, 25);
+  //   doc.autoTable(tableColumn, tableRows, {
+  //     styles: { fontSize: 8 },
+  //     startY: 35,
+  //   });
+  //   doc.addImage(img1, "JPEG", 120, 80, 70, 40);
+  //   doc.save("Contract Details Report.pdf");
+  // };
+
   return (
     <React.Fragment>
       <div className="signup_container">
-        <div className=".signup_form_container">
+        <div className="signup_form_container ">
+          <div class="buttonn">
+            {/* <button
+          type="button"
+          class="btn btn-secondary btn-sm"
+          onClick={() => generatePDF(poRequestManage)}
+        >
+          GenerateReport
+        </button>{" "} */}
+            <br></br>
+          </div>
+          <br />
           <Typography variant="h6" gutterBottom>
-            Order summary
+            <u>Order summary</u>
           </Typography>
+
+          <div style={{ display: "none" }}>
+            {poRequestManage.map((productPO) =>
+              sum.push(productPO.itemPrice * productPO.itemQuantity)
+            )}
+          </div>
+
           <List disablePadding>
-            {products.map((product) => (
-              <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
-                <ListItemText primary={product.name} secondary={product.desc} />
-                <Typography variant="body2">{product.price}</Typography>
+            {poRequestManage.map((product) => (
+              <ListItem className={classes.listItem} key={product.itemName}>
+                <ListItemText
+                  primary={product.itemName}
+                  secondary={
+                    "Rs: " +
+                    product.itemPrice +
+                    " X " +
+                    product.itemQuantity +
+                    " items"
+                  }
+                />
+                <Typography variant="body2">
+                  {"Rs: " + product.itemPrice * product.itemQuantity}
+                </Typography>
               </ListItem>
             ))}
-
-            <ListItem sx={{ py: 1, px: 0 }}>
+            <ListItem className={classes.listItem}>
               <ListItemText primary="Total" />
-              <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
-                $34.06
+              <Typography variant="subtitle1" className={classes.total}>
+                {"Rs: " + sum.reduce((a, b) => a + b, 0)}
               </Typography>
             </ListItem>
           </List>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
-              <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
-                Shipping
+              <Typography variant="h6" gutterBottom className={classes.title}>
+                <u>Delivery Details</u>
               </Typography>
-              <Typography gutterBottom>John Smith</Typography>
+              <br />
+              <Typography gutterBottom>Wickrama Super</Typography>
               <Typography gutterBottom>{addresses.join(", ")}</Typography>
             </Grid>
             <Grid item container direction="column" xs={12} sm={6}>
-              <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
-                Payment details
+              <Typography variant="h6" gutterBottom className={classes.title}>
+                <u>Payment Details</u>
               </Typography>
+              <br />
               <Grid container>
                 {payments.map((payment) => (
                   <React.Fragment key={payment.name}>
