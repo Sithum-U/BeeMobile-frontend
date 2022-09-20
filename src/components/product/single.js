8f959a56b9a5fee84d28a1b72cbcfe5ce3d4f370
Buddhini-123 <buddhiniup123@gmail.com>
2022-09-20 23:17:09 +0530

    single product page updated

--- src/components/product/single.js
+++ src/components/product/single.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 import { Button } from 'react-bootstrap';
 import { useParams } from "react-router-dom";
+import {Link } from "react-router-dom";
 
 const Single = () => {
     const [product, setproduct] = useState([]);
@@ -30,8 +31,12 @@ const Single = () => {
                             
                             <span class="meta">
                                 Category : 
+                                {product.data ?
                                 <h3>{product.data.category}</h3>
+                                : <div></div>}
+                                {product.data ?
                                 <a href="#!"> {product.data.productCode} </a>
+                                : <div></div>}
                                 on August 24, 2022
                             </span>
                         </div>
@@ -39,6 +44,31 @@ const Single = () => {
                 </div>
             </div>
         </div><br/><br/><br/>
+
+        <article class="mb-4">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-md-10 col-lg-8 col-xl-7">
+                    {product.data ?
+                        <p>{product.data.description}</p>
+                    : <div></div>}
+                        <p>
+                            Placeholder text by
+                            <a href="http://spaceipsum.com/">Space Ipsum</a>
+                            &middot; Images by
+                            <a href="https://www.flickr.com/photos/nasacommons/">NASA on The Commons</a>
+                        </p>
+                    </div>  
+                    
+                </div>
+            </div>
+        </article>
+        <Link to="/">
+                    <Button>Pay Now</Button>
+                </Link>
+                <Link to="/">
+                <Button>Add to Cart</Button>
+                </Link>
         </div>
 
     );
