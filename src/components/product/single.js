734921a2fea7a2b8ea3e70759811704d4aa8b8e9
Buddhini-123 <buddhiniup123@gmail.com>
2022-09-20 23:28:48 +0530

    product single page css added

--- src/components/product/single.js
+++ src/components/product/single.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 import { Button } from 'react-bootstrap';
 import { useParams } from "react-router-dom";
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Single = () => {
     const [product, setproduct] = useState([]);
@@ -20,55 +20,56 @@ const Single = () => {
     return (
         <div>
             <div class="masthead" >
-            <div class="container position-relative px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-md-10 col-lg-8 col-xl-7">
-                        <div class="post-heading">
-                        {product.data ?
-                            <h1>{product.data.productName}</h1>
-                        : <div></div>}
-                            
-                            
-                            <span class="meta">
-                                Category : 
+                <div class="container position-relative px-4 px-lg-5">
+                    <div class="row gx-4 gx-lg-5 justify-content-center">
+                        <div class="col-md-10 col-lg-8 col-xl-7">
+                            <div class="post-heading">
                                 {product.data ?
-                                <h3>{product.data.category}</h3>
-                                : <div></div>}
-                                {product.data ?
-                                <a href="#!"> {product.data.productCode} </a>
-                                : <div></div>}
-                                on August 24, 2022
-                            </span>
+                                    <h1>{product.data.productName}</h1>
+                                    : <div></div>}
+
+
+                                <span class="meta">
+                                    Category :
+                                    {product.data ?
+                                        <h3>{product.data.category}</h3>
+                                        : <div></div>}
+                                    {product.data ?
+                                        <a href="#!"> {product.data.productCode} </a>
+                                        : <div></div>}
+                                    on August 24, 2022
+                                    <br />
+                                    <Link to="/">
+                                        <Button variant="danger">Pay Now</Button>
+                                    </Link>&nbsp;&nbsp;
+                                    <Link to="/">
+                                        <Button variant="success">Add to Cart</Button>
+                                    </Link>
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div><br/><br/><br/>
+            </div><br /><br /><br />
 
-        <article class="mb-4">
-            <div class="container px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-md-10 col-lg-8 col-xl-7">
-                    {product.data ?
-                        <p>{product.data.description}</p>
-                    : <div></div>}
-                        <p>
-                            Placeholder text by
-                            <a href="http://spaceipsum.com/">Space Ipsum</a>
-                            &middot; Images by
-                            <a href="https://www.flickr.com/photos/nasacommons/">NASA on The Commons</a>
-                        </p>
-                    </div>  
-                    
+            <article class="mb-4">
+                <div class="container px-4 px-lg-5">
+                    <div class="row gx-4 gx-lg-5 justify-content-center">
+                        <div class="col-md-10 col-lg-8 col-xl-7">
+                            {product.data ?
+                                <p>{product.data.description}</p>
+                                : <div></div>}
+                            <p>
+                                Placeholder text by
+                                <a href="http://spaceipsum.com/">Space Ipsum</a>
+                                &middot; Images by
+                                <a href="https://www.flickr.com/photos/nasacommons/">NASA on The Commons</a>
+                            </p>
+                        </div>
+
+                    </div>
                 </div>
-            </div>
-        </article>
-        <Link to="/">
-                    <Button>Pay Now</Button>
-                </Link>
-                <Link to="/">
-                <Button>Add to Cart</Button>
-                </Link>
+            </article>
         </div>
 
     );
